31624533d4b6924a80738ec70e0be87e50aa152f
Liam <ltwirth@asu.edu>
2025-04-10 21:43:38 -0700

    mazecetric

---
+++ consts.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# Reset
+NC='\033[0m' # No Color (reset to default)
+
+set -a
+# Regular Colors
+BLACK='\033[0;30m'   # Black
+RED='\033[0;31m'     # Red
+GREEN='\033[0;32m'   # Green
+YELLOW='\033[0;33m'  # Yellow
+BLUE='\033[0;34m'    # Blue
+MAGENTA='\033[0;35m' # Magenta
+CYAN='\033[0;36m'    # Cyan
+WHITE='\033[0;37m'   # White
+
+# Bright Colors
+BRIGHT_BLACK='\033[0;90m'   # Bright Black (Gray)
+BRIGHT_RED='\033[0;91m'     # Bright Red
+BRIGHT_GREEN='\033[0;92m'   # Bright Green
+BRIGHT_YELLOW='\033[0;93m'  # Bright Yellow
+BRIGHT_BLUE='\033[0;94m'    # Bright Blue
+BRIGHT_MAGENTA='\033[0;95m' # Bright Magenta
+BRIGHT_CYAN='\033[0;96m'    # Bright Cyan
+BRIGHT_WHITE='\033[0;97m'   # Bright White
+
+# Regular Background Colors
+BG_BLACK='\033[0;40m'   # Black
+BG_RED='\033[0;41m'     # Red
+BG_GREEN='\033[0;42m'   # Green
+BG_YELLOW='\033[0;43m'  # Yellow
+BG_BLUE='\033[0;44m'    # Blue
+BG_MAGENTA='\033[0;45m' # Magenta
+BG_CYAN='\033[0;46m'    # Cyan
+BG_WHITE='\033[0;47m'   # White
+
+# Bright Background Colors
+BG_BRIGHT_BLACK='\033[0;100m'   # Bright Black (Gray)
+BG_BRIGHT_RED='\033[0;101m'     # Bright Red
+BG_BRIGHT_GREEN='\033[0;102m'   # Bright Green
+BG_BRIGHT_YELLOW='\033[0;103m'  # Bright Yellow
+BG_BRIGHT_BLUE='\033[0;104m'    # Bright Blue
+BG_BRIGHT_MAGENTA='\033[0;105m' # Bright Magenta
+BG_BRIGHT_CYAN='\033[0;106m'    # Bright Cyan
+BG_BRIGHT_WHITE='\033[0;107m'   # Bright White
